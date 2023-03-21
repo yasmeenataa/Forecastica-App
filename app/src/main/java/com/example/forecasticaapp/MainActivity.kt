027283a0a9a88b6dd82fdda54757cdc0dd5df97b
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-03-21 18:28:32 +0200

    handle the navigation drawer on main activity

--- app/src/main/java/com/example/forecasticaapp/MainActivity.kt
+++ app/src/main/java/com/example/forecasticaapp/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.forecasticaapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.forecasticaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +15,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val actionBar = supportActionBar
+        actionBar?.setHomeAsUpIndicator(R.drawable.menu)
+        actionBar!!.setDisplayShowCustomEnabled(true)
+        actionBar!!.setDisplayHomeAsUpEnabled(true)
+        binding.navegatorLayout.itemIconTintList = null
+        val navController = findNavController(this, R.id.nav_host_fragment)
+        setupWithNavController(binding.navegatorLayout, navController)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                binding.drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                binding.drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
