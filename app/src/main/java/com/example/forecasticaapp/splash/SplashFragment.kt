2e393774378739c3a452d7b10a4894edf8b431ca
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-03-24 13:08:43 +0200

    design SplashFragment

---
+++ app/src/main/java/com/example/forecasticaapp/splash/SplashFragment.kt
@@ -0,0 +1,45 @@
+package com.example.forecasticaapp.splash
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.Navigation.findNavController
+import com.example.forecasticaapp.R
+
+
+class SplashFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+        Handler().postDelayed({
+            findNavController(view)
+                .navigate(R.id.action_splashFragment_to_homeFragment2)
+
+        }, 5000)
+
+
+    }
+
+
+}
