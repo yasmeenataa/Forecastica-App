5c92ab87a9885f6138f49a1be7ad8902b6962581
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-03-25 17:16:52 +0200

    design favourite fragment

---
+++ app/src/main/java/com/example/forecasticaapp/favourite/FavoriteFragment.kt
@@ -0,0 +1,38 @@
+package com.example.forecasticaapp.favourite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.forecasticaapp.databinding.FragmentFavoriteBinding
+
+
+class FavoriteFragment : Fragment() {
+    lateinit var binding: FragmentFavoriteBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (context as AppCompatActivity).supportActionBar?.title = "Favorite"
+    }
+}
