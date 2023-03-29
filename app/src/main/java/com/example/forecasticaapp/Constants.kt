75b61b3603841e257616ba3b237ffcfa1e3221c1
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-03-29 14:38:32 +0200

    move Constants.kt to utils package

--- app/src/main/java/com/example/forecasticaapp/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.example.forecasticaapp
-
-object Constants{
-    const val LOCATION = "LOCATION"
-    enum class ENUM_LOCATION(){Map,Gps}
-    const val NOTIFICATIONS = "NOTIFICATIONS"
-    enum class ENUM_NOTIFICATIONS(){Enabled,Disabled}
-}
