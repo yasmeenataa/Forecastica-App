0bdacbdd6fff73b83bf825942cd4e32473a9ec7f
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-03-30 23:05:10 +0200

    add function to check connection of internet

--- app/src/main/java/com/example/forecasticaapp/utils/CommonOperations.kt
+++ app/src/main/java/com/example/forecasticaapp/utils/CommonOperations.kt
@@ -88,27 +88,6 @@ fun getImageIcon(icon: String): Int {
 fun isConnected(context: Context): Boolean {
     val connectivityManager =
         context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        val capabilities =
-            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-        if (capabilities != null) {
-            when {
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
-                    return true
-                }
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
-                    return true
-                }
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
-                    return true
-                }
-            }
-        }
-    } else {
-        val activeNetworkInfo = connectivityManager.activeNetworkInfo
-        if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
-            return true
-        }
-    }
-    return false
+    return connectivityManager.activeNetworkInfo != null && connectivityManager.activeNetworkInfo!!
+        .isConnected
 }
