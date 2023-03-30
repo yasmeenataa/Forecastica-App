254b09cb4bb86db938c62e012bc2ed990ceaba78
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-03-30 03:30:33 +0200

    create adapter for day row

---
+++ app/src/main/java/com/example/forecasticaapp/homePage/view/DaysAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.forecasticaapp.homePage.view
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.forecasticaapp.utils.Constants
+import com.example.forecasticaapp.databinding.RowDayBinding
+import com.example.forecasticaapp.models.Daily
+import com.example.forecasticaapp.utils.getDayFormat
+import com.example.forecasticaapp.utils.getImageIcon
+import com.example.forecasticaapp.utils.getTemperatureUnit
+
+class DaysAdapter(private var daysList: MutableList<Daily>, var context: Context) :
+    RecyclerView.Adapter<DaysAdapter.ViewHolder>() {
+    private lateinit var binding: RowDayBinding
+    private val sharedPreference: SharedPreferences = context.getSharedPreferences(Constants.SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE)
+    private val language =  sharedPreference.getString(Constants.LANGUAGE,"en") !!
+
+    fun setList(list: List<Daily>) {
+        daysList = list as MutableList<Daily>
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater: LayoutInflater =
+            parent.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        binding = RowDayBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = daysList.size
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val current = daysList[position]
+        holder.binding.txtDayName.text= getDayFormat(current.dt,language)
+        holder.binding.iconDay.setImageResource(getImageIcon(current.weather[0].icon))
+        holder.binding.txtDescDay.text=current.weather[0].description
+        holder.binding.txtMaxTempDay.text="${current.temp.max}${getTemperatureUnit(context)}"
+        holder.binding.txtMinTempDay.text="${current.temp.min}${getTemperatureUnit(context)}"
+    }
+    inner class ViewHolder(var binding: RowDayBinding) : RecyclerView.ViewHolder(binding.root)
+
+
+}
