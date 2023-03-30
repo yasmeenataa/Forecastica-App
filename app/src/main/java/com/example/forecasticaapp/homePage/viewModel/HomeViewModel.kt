7e926630a51459c9c159c2dfb48792bcdf9467b6
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-03-30 23:07:45 +0200

    add functions of room to the home view model insert ,delete and get current Weather

--- app/src/main/java/com/example/forecasticaapp/homePage/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/forecasticaapp/homePage/viewModel/HomeViewModel.kt
@@ -2,16 +2,20 @@ package com.example.forecasticaapp.homePage.viewModel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.forecasticaapp.models.OneCallResponse
 import com.example.forecasticaapp.models.RepositoryInterface
 import com.example.forecasticaapp.network.APIState
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 
 class HomeViewModel(private val _irpo: RepositoryInterface) : ViewModel() {
     private var oneCallResponse = MutableStateFlow<APIState>(APIState.Loading)
-    val data=oneCallResponse
+    val data = oneCallResponse
+    private var _currentWeather: MutableStateFlow<List<OneCallResponse>> = MutableStateFlow<List<OneCallResponse>>(ArrayList())
+    val currentWeather =_currentWeather
 
     fun getOneCallResponse(
         lat: Double,
@@ -23,6 +27,25 @@ class HomeViewModel(private val _irpo: RepositoryInterface) : ViewModel() {
             _irpo.getOneCallResponse(lat, lon, units, lang)
                 ?.catch { e -> oneCallResponse.value = APIState.Failure(e) }
                 ?.collect { data -> oneCallResponse.value = APIState.Success(data) }
+        }
+    }
+
+    fun insertCurrentWeather(weather: OneCallResponse?) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _irpo.insertCurrentWeather(weather)
+        }
+    }
+
+    fun getCurrentWeather() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _currentWeather.value =  _irpo.getCurrentWeather()
+
+        }
+    }
+
+    fun deleteCurrentWeather() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _irpo.deleteCurrentWeather()
         }
     }
 }
