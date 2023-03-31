196112372d35ae19ed931ae36f6baf86cc619add
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-04-01 01:01:11 +0200

    update currentWeatherDAO to make select function return Flow

--- app/src/main/java/com/example/forecasticaapp/database/CurrentWeatherDAO.kt
+++ app/src/main/java/com/example/forecasticaapp/database/CurrentWeatherDAO.kt
@@ -2,16 +2,17 @@ package com.example.forecasticaapp.database
 
 import androidx.room.*
 import com.example.forecasticaapp.models.OneCallResponse
+import com.example.forecasticaapp.models.RoomHomePojo
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CurrentWeatherDAO {
     @Query("Select * from CurrentWeather")
-    suspend fun  getCurrentWeather(): List<OneCallResponse>
+     fun  getCurrentWeather(): Flow<List<RoomHomePojo>>
 //    @Query("Select * from CurrentWeather where id=1")
 //    fun getCurrentWeather(): Flow<OneCallResponse>
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(weather: OneCallResponse?)
+    suspend fun insertCurrentWeather(weather: RoomHomePojo?)
     @Query("Delete from CurrentWeather")
-    suspend fun delete()
+    suspend fun deleteCurrentWeather()
 }
