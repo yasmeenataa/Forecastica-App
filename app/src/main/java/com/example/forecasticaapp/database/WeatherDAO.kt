a2489325715ec0c73f5385f3bbdf4fb1a625e195
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-04-04 10:26:08 +0200

    update the WeatherDAO.kt

--- app/src/main/java/com/example/forecasticaapp/database/WeatherDAO.kt
+++ app/src/main/java/com/example/forecasticaapp/database/WeatherDAO.kt
@@ -1,17 +1,18 @@
 package com.example.forecasticaapp.database
 
 import androidx.room.*
+import com.example.forecasticaapp.models.OneCallResponse
 import com.example.forecasticaapp.models.RoomHomePojo
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CurrentWeatherDAO {
     @Query("Select * from CurrentWeather")
-     fun  getCurrentWeather(): Flow<List<RoomHomePojo>>
+     fun  getCurrentWeather(): Flow<List<OneCallResponse>>
 //    @Query("Select * from CurrentWeather where id=1")
 //    fun getCurrentWeather(): Flow<OneCallResponse>
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertCurrentWeather(weather: RoomHomePojo?)
+    suspend fun insertCurrentWeather(weather: OneCallResponse?)
     @Query("Delete from CurrentWeather")
     suspend fun deleteCurrentWeather()
 }
